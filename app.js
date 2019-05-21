5f03535b53ff73ad2be094cae93dce556b930ced
Taylor Artemenko <taylorartemenko6@gmail.com>
2019-05-21 16:11:49 -0700

    fixed the login

--- app.js
+++ app.js
@@ -444,6 +444,12 @@ app.post('/login', (request, response) => {
                         loginlogoutButton: '<li class="nav-item" id="cart"><a href="/logout" class="nav-link">Logout</a></li>'
                     });
                 } else {
+                    continue;
+                }
+            } else {
+                console.log(result.length);
+                console.log(i);
+                if ((i + 1) === result.length) {
                     var cart = [{
                         title: "No Items",
                         price: 0,
@@ -467,26 +473,9 @@ app.post('/login', (request, response) => {
                             '    });\n' +
                             '</script>'
                     });
+                } else {
+                    continue;
                 }
-            } else {
-                response.render('landing.hbs', {
-                    cart: cart,
-                    sub_total: Math.round(arr.arrSum(sub_total) * 100) / 100,
-                    popup: '<script>\n' +
-                        '    $(document).ready(function () {\n' +
-                        '        var mymodal = document.querySelector(\'.popup6\');\n' +
-                        '        mymodal.style.visibility = \'visible\';\n' +
-                        '        mymodal.style.opacity = \'1\';\n' +
-                        '    })\n' +
-                        '</script>',
-                    loginlogoutButton: '<li class="nav-item" id="loginbutton"><a href="#" class="nav-link" data-toggle="modal" data-target="#login">Login</a></li>',
-                    imgTag: '<img id="captchapng" src="/vcode" alt="Smiley face" height="30" width="80">',
-                    modal: '<script type="text/javascript">\n' +
-                        '    $(window).on(\'load\',function(){\n' +
-                        '        $(\'#login\').modal(\'show\');\n' +
-                        '    });\n' +
-                        '</script>'
-                });
             }
         }
     });
