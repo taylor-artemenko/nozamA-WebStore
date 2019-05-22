140f05e4641a2fc95dd2510e90f91d472b19be8f
Taylor Artemenko <taylorartemenko6@gmail.com>
2019-05-21 20:21:12 -0700

    AHH

--- utils.js
+++ utils.js
@@ -6,6 +6,7 @@ module.exports.getDb = () => {
     return _db;
 };
 
+
 module.exports.init = function(callback) {
     MongoClient.connect('mongodb://heroku_8vt9b6p4:Donkey6123@ds259586.mlab.com:59586/heroku_8vt9b6p4', (err, client) => {
         if (err) {
