744dcde068c10aa761e4b897ea94b3810f0e7ad3
Taylor Artemenko <taylorartemenko6@gmail.com>
2019-05-21 20:15:09 -0700

    heroku

--- utils.js
+++ utils.js
@@ -7,7 +7,7 @@ module.exports.getDb = () => {
 };
 
 module.exports.init = function(callback) {
-    MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+    MongoClient.connect('mongodb://heroku_8vt9b6p4:Donkey6123@ds259586.mlab.com:59586/heroku_8vt9b6p4', (err, client) => {
         if (err) {
             return console.log('Unable to connect to DB')
         }
